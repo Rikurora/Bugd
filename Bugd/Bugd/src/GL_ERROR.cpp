8f0457c41bc4dccd9f23bbf7877c8ed47766926d
Anthony Langford <72790908+ALangford98@users.noreply.github.com>
2021-05-31 12:27:35 +0200

    Add files via upload

---
+++ Bugd/Bugd/src/GL_ERROR.cpp
@@ -0,0 +1,17 @@
+#include "GL_ERROR.h"
+#include <iostream>
+
+void GLClearError()
+{
+    while (glGetError() != glGetError());
+}
+
+bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ": " << line << std::endl;
+        return false;
+    }
+    return true;
+}
